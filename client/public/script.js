3bd7b12b68bcc4f4dca95f9d04939e895e322946
stszandra <snszandra@gmail.com>
2023-02-28 19:45:41 +0100

    created the starter files from an other project

---
+++ client/public/script.js
@@ -0,0 +1,7 @@
+import { products } from "/data.js";
+
+const loadEvent = function () {
+  //document.getElementById("root").insertAdjacentHTML("beforeend", createDiv4(products, "and", findComposers));
+};
+
+window.addEventListener("load", loadEvent);
