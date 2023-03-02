b967a58a4fa3c90d8ded5b119c9b9f958fc61c75
stszandra <snszandra@gmail.com>
2023-03-02 19:29:59 +0100

    class and race select fields done

--- client/public/script.js
+++ client/public/script.js
@@ -1,7 +1,37 @@
-import { products } from "/data.js";
+import { data } from "/data.js";
 
 const loadEvent = function () {
-  //document.getElementById("root").insertAdjacentHTML("beforeend", createDiv4(products, "and", findComposers));
+  document.getElementById("class-second").insertAdjacentHTML("beforeend", createClassOptions());
+  document.getElementById("class-second").addEventListener("change", createRaceOptions);
 };
 
 window.addEventListener("load", loadEvent);
+
+function createClassOptions() {
+  let options = `<option>Choose your class</option>`;
+  let classNames = data.classes;
+  classNames.forEach((className) => {
+    options += `
+    <option>${className}</option>
+    `;
+  });
+  return options;
+}
+
+function createRaceOptions() {
+  document.getElementById("race-second").innerHTML = "";
+  let className = document.getElementById("class-second").value;
+  let options = `<option>Choose your race</option>`;
+  data.races.forEach((byClass) => {
+    if (className === byClass.byClassName) {
+      byClass.allowedRaces.forEach((race) => {
+        options += `<option>${race}`;
+      });
+      byClass.allowedRacesByDM.forEach((race) => {
+        options += `<option>${race} *</option>`;
+      });
+    }
+  });
+  console.log(options);
+  document.getElementById("race-second").insertAdjacentHTML("beforeend", options);
+}
