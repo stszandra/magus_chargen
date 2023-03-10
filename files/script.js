ed12487453d038019c237af91c5617c52bcadd34
stszandra <snszandra@gmail.com>
2023-03-10 06:36:56 +0100

    age and apparent age done

--- files/script.js
+++ files/script.js
@@ -64,6 +64,9 @@ function loadEvent() {
   insertSpinnerForAge();
   raceSelectElement.addEventListener("change", setValuesOfAgeSpinner);
   validateRealAge();
+  calculateAgeCategory();
+  //ApparentAge
+  calculateApparentAge();
 }
 
 //Class options:
@@ -128,14 +131,19 @@ function createRaceOptions() {
   raceSelectElement.insertAdjacentHTML("beforeend", options);
 }
 
+function calculateChosenRace() {
+  let chosenRace = raceSelectElement.value;
+  if (chosenRace.charAt(chosenRace.length - 1) === "*") {
+    chosenRace = chosenRace.slice(0, chosenRace.length - 2);
+  }
+  return chosenRace;
+}
+
 function showInfoForSelectedRace() {
   let infoMessage = `<p>No info</p>`;
 
   $("#race-infoButton").click(function () {
-    let chosenRace = raceSelectElement.value;
-    if (chosenRace.charAt(chosenRace.length - 1) === "*") {
-      chosenRace = chosenRace.slice(0, chosenRace.length - 2);
-    }
+    let chosenRace = calculateChosenRace();
     data.races.forEach((raceName) => {
       if (raceName.name === chosenRace) {
         infoMessage = `<p class="info-box-message">${raceName.info}</p>`;
@@ -157,10 +165,7 @@ function showInfoForSelectedRace() {
 //Alignment options:
 function createAlignmentOptions() {
   alignmentSelectElement.innerHTML = "";
-  let chosenRace = raceSelectElement.value;
-  if (chosenRace.charAt(chosenRace.length - 1) === "*") {
-    chosenRace = chosenRace.slice(0, chosenRace.length - 2);
-  }
+  let chosenRace = calculateChosenRace();
   let options = `<option>Choose your alignment</option>`;
 
   data.alignments.forEach((alignment) => {
@@ -265,16 +270,16 @@ function insertSpinnerForAge() {
 }
 
 function setValuesOfAgeSpinner() {
-  let chosenRace = raceSelectElement.value;
+  let chosenRace = calculateChosenRace();
   data.races.forEach((race) => {
     if (race.name === chosenRace) {
       minAge = race.age.category1[0];
       maxAge = race.age.category6[0];
     }
   });
-  $("#realAge-second").spinner("value", minAge);
   $("#realAge-second").spinner("option", "min", minAge);
   $("#realAge-second").spinner("option", "max", maxAge);
+  $("#realAge-second").spinner("value", minAge);
 }
 
 function validateRealAge() {
@@ -288,10 +293,22 @@ function validateRealAge() {
     })
     .on("spinchange", function () {
       chosenAge = $(this).spinner("value");
-      if (chosenAge < minAge) {
+      if (typeof chosenAge !== "number") {
+        $("#realAge-second").spinner("value", minAge);
+        const infoMessage = `<p>Numbers only, please!</p>`;
+        $(infoMessage).dialog({
+          modal: true,
+          width: 400,
+          title: "Invalid input!",
+          buttons: {
+            OK: function () {
+              $(this).dialog("close");
+            },
+          },
+        });
+      } else if (chosenAge < minAge) {
         $("#realAge-second").spinner("value", minAge);
         const infoMessage = `<p>You can't be younger than ${minAge} if you want to survive your first real combat encounter.</p>`;
-        //const infoMessage = "You can't be that young!";
         $(infoMessage).dialog({
           modal: true,
           width: 400,
@@ -302,11 +319,9 @@ function validateRealAge() {
             },
           },
         });
-      }
-      if (chosenAge > maxAge) {
+      } else if (chosenAge > maxAge) {
         $("#realAge-second").spinner("value", maxAge);
         const infoMessage = `<p>You can't be older than ${maxAge} if you still want to actively partake in combat encounters.</p>`;
-        //const infoMessage = "You can't be that old!";
         $(infoMessage).dialog({
           modal: true,
           width: 400,
@@ -324,4 +339,130 @@ function validateRealAge() {
       $(this).trigger("change").trigger("spinchange");
     });*/
   //No good, because when I type 14, the value is at first 1, then its 14, so it will be useless for validation.
+}
+
+function calculateAgeCategory() {
+  $("#realAge-second")
+    .spinner({
+      spin: function (event, ui) {
+        $(this).val(ui.value).trigger("change").trigger("spinchange");
+      },
+    })
+    .on("spinchange", function () {
+      let chosenRace = calculateChosenRace();
+      let chosenAge = $(this).spinner("value");
+      data.races.forEach((race) => {
+        if (race.name === chosenRace) {
+          if (chosenAge <= race.age.category1[1]) {
+            insertAgeCategory("Teenager");
+          } else if (chosenAge <= race.age.category2[1]) {
+            insertAgeCategory("Young adult");
+          } else if (chosenAge <= race.age.category3[1]) {
+            insertAgeCategory("Adult");
+          } else if (chosenAge <= race.age.category4[1]) {
+            insertAgeCategory("Middle-aged adult");
+          } else if (chosenAge <= race.age.category5[1]) {
+            insertAgeCategory("Old adult");
+          } else {
+            insertAgeCategory("Elderly");
+          }
+        }
+      });
+    });
+}
+
+function insertAgeCategory(text) {
+  document.getElementById("realAge-third").innerHTML = text;
+}
+
+function calculateApparentAge() {
+  let apparentAge;
+  $("#realAge-second")
+    .spinner({
+      spin: function (event, ui) {
+        $(this).val(ui.value).trigger("change").trigger("spinchange");
+      },
+    })
+    .on("spinchange", function () {
+      let chosenRace = calculateChosenRace();
+      let chosenAge = $(this).spinner("value");
+      let chosenRaceAgeCategoryBottom;
+      let chosenRaceAgeCategoryTop;
+      let humanAgeCategoryBottom;
+      let humanAgeCategoryTop;
+      data.races.forEach((race) => {
+        if (race.name === chosenRace) {
+          if (chosenAge <= race.age.category1[1]) {
+            chosenRaceAgeCategoryBottom = race.age.category1[0];
+            chosenRaceAgeCategoryTop = race.age.category1[1];
+            humanAgeCategoryBottom = data.races[0].age.category1[0];
+            humanAgeCategoryTop = data.races[0].age.category1[1];
+            apparentAge = AgeLinearInterpolation(
+              chosenRaceAgeCategoryBottom,
+              chosenRaceAgeCategoryTop,
+              chosenAge,
+              humanAgeCategoryBottom,
+              humanAgeCategoryTop
+            );
+          } else if (chosenAge <= race.age.category2[1]) {
+            chosenRaceAgeCategoryBottom = race.age.category2[0];
+            chosenRaceAgeCategoryTop = race.age.category2[1];
+            humanAgeCategoryBottom = data.races[0].age.category2[0];
+            humanAgeCategoryTop = data.races[0].age.category2[1];
+            apparentAge = AgeLinearInterpolation(
+              chosenRaceAgeCategoryBottom,
+              chosenRaceAgeCategoryTop,
+              chosenAge,
+              humanAgeCategoryBottom,
+              humanAgeCategoryTop
+            );
+          } else if (chosenAge <= race.age.category3[1]) {
+            chosenRaceAgeCategoryBottom = race.age.category3[0];
+            chosenRaceAgeCategoryTop = race.age.category3[1];
+            humanAgeCategoryBottom = data.races[0].age.category3[0];
+            humanAgeCategoryTop = data.races[0].age.category3[1];
+            apparentAge = AgeLinearInterpolation(
+              chosenRaceAgeCategoryBottom,
+              chosenRaceAgeCategoryTop,
+              chosenAge,
+              humanAgeCategoryBottom,
+              humanAgeCategoryTop
+            );
+          } else if (chosenAge <= race.age.category4[1]) {
+            chosenRaceAgeCategoryBottom = race.age.category4[0];
+            chosenRaceAgeCategoryTop = race.age.category4[1];
+            humanAgeCategoryBottom = data.races[0].age.category4[0];
+            humanAgeCategoryTop = data.races[0].age.category4[1];
+            apparentAge = AgeLinearInterpolation(
+              chosenRaceAgeCategoryBottom,
+              chosenRaceAgeCategoryTop,
+              chosenAge,
+              humanAgeCategoryBottom,
+              humanAgeCategoryTop
+            );
+          } else if (chosenAge <= race.age.category5[1]) {
+            chosenRaceAgeCategoryBottom = race.age.category5[0];
+            chosenRaceAgeCategoryTop = race.age.category5[1];
+            humanAgeCategoryBottom = data.races[0].age.category5[0];
+            humanAgeCategoryTop = data.races[0].age.category5[1];
+            apparentAge = AgeLinearInterpolation(
+              chosenRaceAgeCategoryBottom,
+              chosenRaceAgeCategoryTop,
+              chosenAge,
+              humanAgeCategoryBottom,
+              humanAgeCategoryTop
+            );
+          } else {
+            apparentAge = data.races[0].age.category6[0];
+          }
+        }
+      });
+      apparentAge = Math.round(apparentAge);
+      document.getElementById("apparentAge-second").innerHTML = `You look ${apparentAge}`;
+    });
+  return apparentAge;
+}
+
+function AgeLinearInterpolation(ageX1, ageX2, ageX3, ageY1, ageY2) {
+  return ((ageY2 - ageY1) / (ageX2 - ageX1)) * (ageX3 - ageX1) + ageY1;
 }
